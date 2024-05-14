9b9fecd6e401d74dbf50e8eb5d94cd81fc0cd295
kelvinligs <kelvin.li.gs@gmail.com>
2024-05-14 12:31:24 +0800

    feat(test):clean up fixture codes

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,9 @@
 import pytest
+from concurrent import futures
 import grpc
 
 from app import app as flask_app
 from grpclib import grpcserver, useraccount_pb2_grpc
-import grpclib.useraccount_pb2 as up
 
 @pytest.fixture
 def app():
@@ -12,10 +12,11 @@ def app():
 
 @pytest.fixture
 def grpc_server():
-    grpcserver.serve()
-
-# @pytest.fixture
-# def grpc_client():
-#     with grpc.insecure_channel(f'localhost:50051') as channel:
-#         stub = useraccount_pb2_grpc.UserAccountStub(channel)
-#         response = stub.LoginUser(up.User(name='Jack'))
+    server = grpc.server((futures.ThreadPoolExecutor(max_workers=10)))
+    useraccount_pb2_grpc.add_UserAccountServicer_to_server(grpcserver.UserAccountService(), server)
+    server.add_insecure_port(f'[::]:50051')
+    print('gRPC server starting...')
+    server.start()
+    yield server
+    print('gRPC server stopping...')
+    server.stop(grace=5)
--- test/user_test.py
+++ test/user_test.py
@@ -1,10 +1,8 @@
 import grpc
 import pytest
-from concurrent import futures
 
 from app import app as flask_app
-from grpclib import grpcserver, useraccount_pb2_grpc, useraccount_pb2
-import grpclib.grpcserver
+from grpclib import useraccount_pb2_grpc, useraccount_pb2
 
 
 @pytest.fixture
@@ -12,19 +10,11 @@ def app():
     yield flask_app
 
 
-@pytest.fixture(autouse=True)
-def grpc_server():
-    server = grpc.server((futures.ThreadPoolExecutor(max_workers=10)))
-    useraccount_pb2_grpc.add_UserAccountServicer_to_server(grpcserver.UserAccountService(), server)
-    server.add_insecure_port(f'[::]:50051')
-    server.start()
-    yield server
-    server.stop(grace=5)
-
-def test_user_login():
+def test_user_login(grpc_server):
     with grpc.insecure_channel(f'localhost:50051') as channel:
         stub = useraccount_pb2_grpc.UserAccountStub(channel)
         response = stub.LoginUser(useraccount_pb2.LoginUserRequest(Email='1@test.com', Password='12345'))
         expected_user = useraccount_pb2.User(UserId=1, FirstName='Tester1', LastName='One', Email='1@test.com')
+        print('Testing...')
         assert response.IsSuccess == True
         assert response.User == expected_user
